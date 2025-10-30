540a8598e67f83730596092c98a122cceee2e2df
Rodrigo Werlin <rodrigowerlin@gmail.com>
2025-10-30 17:57:16 -0300

    ajustes membors

--- app/Http/Controllers/MembrosController.php
+++ app/Http/Controllers/MembrosController.php
@@ -13,10 +13,10 @@ class MembrosController extends Controller {
 	
 	public function content() {
         // Membros
-        $arrParams["filter_params"] = array("codpagina" => 5, "ordem" => array("datapublicacao" => "ASC"), "idioma" => Config::get('app.locale'), "limite" => 1, "imglimite" => 1);
+        $arrParams["filter_params"] = array("codpagina" => 20, "ordem" => array("datapublicacao" => "ASC"), "idioma" => Config::get('app.locale'), "limite" => 999, "imglimite" => 1);
         $pub = new SequenceServiceModel();
         $arr_membros = $pub->connect($arrParams);
-    
+
         return view("pages.membros")->with("arr_loja", $this->model_loja->result)->with("arr_meta", $this->model_meta->result)->with("arr_enderecos", $this->enderecos)->with("arr_boxes_rodape", $this->boxesRodape)->with("arr_membros", $arr_membros);
     }
 
--- resources/views/pages/membros.blade.php
+++ resources/views/pages/membros.blade.php
@@ -7,17 +7,23 @@
                 <h1>@lang('content.membros.titulo')</h1>
             </div>
             <ul class="members__list">
-                @for($i = 0; $i < 6; $i++)
+                
+                @for($i = 0; $i < count($arr_membros); $i++)
                 <li class="members__item">
-                    <img class="members__img" src="https://i0.wp.com/wlink.org/wp-content/uploads/2024/09/1.png?w=1080&ssl=1" alt="Dr. Manny Fernandez Jr.">
-                    <h2 class="members__name">Dr. Manny Fernandez Jr.</h2>
-                    <p>Founder & CEO</p>
+                    
+                    @for($i2 = 0; $i2 < $arr_membros[$i]->countimgs; $i2++)
+                        <img class="members__img" src="{{ Util::getLinkImg($arr_membros[$i]->imgs[$i2]->codfotocadastro, 'gd', Util::seoImgLink($arr_membros[$i]->imgs[$i2], $arr_membros[$i]->titulo))}} " alt="{{ Util::getPropFromArray($arr_membros, $i, 'titulo') }}">
+                    @endfor
+
+                    <h2 class="members__name">{{ Util::getPropFromArray($arr_membros, $i, 'titulo') }}</h2>
+                    <p>{{ Util::getPropFromArray($arr_membros, $i, 'subtitulo') }}</p>
                     <details class="members__details">
                         <summary class="members__details__title">@lang('content.membros.botao-detalhes')</summary>
-                        <p class="members__details__content">Dr. Manny Fernandez graduated with a Masters in Theology in 1983 and a Doctorate of Ministry from Dallas Theological Seminary in 1996. He founded World Link Ministries and the European Seminary of Theological Formation and Evangelization in Madrid Spain in 1991. He continues to serve as Chairman and CEO of WLM as he oversees the expansion of the ministry which has spread globally. Dr. Manny is a member at First Baptist Church of Dallas. He is married to Glenda and has three children and nine grandchildren.</p>
+                        <p class="members__details__content">{{ Util::getPropFromArray($arr_membros, $i, 'texto') }}</p>
                     </details>
                 </li>
                 @endfor
+
             </ul>
         </section>
     </main>
